59f8b388664043d924e35d1337f1ed63130fe9da
voa9bh <andreas.vorgeitz@me.com>
2024-12-16 15:13:11 +0100

    day16 part 1

--- src/main/kotlin/org/vorgi/advent2024/day16/Day16.kt
+++ src/main/kotlin/org/vorgi/advent2024/day16/Day16.kt
@@ -4,6 +4,8 @@ import org.vorgi.org.vorgi.Utils
 import org.vorgi.org.vorgi.advent2024.day6.CharGrid
 import org.vorgi.org.vorgi.advent2024.day6.Direction
 import org.vorgi.org.vorgi.advent2024.day6.Point
+import java.util.LinkedList
+import java.util.Queue
 import kotlin.math.abs
 
 class Day16 {
@@ -80,21 +82,55 @@ class Day16 {
   }
 
   private fun findPaths(maze: CharGrid, startPoint: Point, visitedPoints: MutableList<Point>): List<List<Point>> {
+    val queue: Queue<MutableList<Point>> = LinkedList()
+    queue.offer(listOf(startPoint).toMutableList())
     val result = mutableListOf<List<Point>>()
 
-    visitedPoints += startPoint
+    while (queue.isNotEmpty()) {
+      val currentPath = queue.poll()
+      val currentPoint = currentPath.last()
 
-    for (direction in Direction.entries) {
-      val newPoint = startPoint + direction
-      if (maze.getAt(newPoint) == '.' && !visitedPoints.contains(newPoint)) {
-        val newVisitedPoints = visitedPoints.toMutableList()
-        result += findPaths(maze, newPoint, newVisitedPoints)
+      for (direction in Direction.entries) {
+        val newPoint = currentPoint + direction
+
+        if (maze.getAt(newPoint) == 'E') {
+          currentPath.add(newPoint)
+          result.add(currentPath)
+          // If you only need to find the *first* path, you can return here:
+          // return result
+          continue //Continue to find all paths
+        }
+
+        if (maze.getAt(newPoint) == '.' && !currentPath.contains(newPoint)) {
+          val newPath = currentPath.toMutableList()
+          newPath.add(newPoint)
+          queue.offer(newPath)
+        }
       }
-      if (maze.getAt(newPoint) == 'E') {
-        visitedPoints += newPoint
-        result += visitedPoints
-        return result
+
+      val endpoints= mutableMapOf<Point,MutableList<Point>>()
+
+      queue.forEach { points ->
+        val lastPoint=points.last()
+        if(!endpoints.contains(lastPoint)) {
+          endpoints[lastPoint] = points
+        } else {
+          val storedValue=evaluatePath().invoke(endpoints[lastPoint]!!)
+          val newValue=evaluatePath().invoke(points)
+          println("removed duplicate")
+          if(newValue<storedValue) {
+            endpoints[lastPoint]=points
+          }
+        }
       }
+
+      queue.clear()
+      endpoints.values.forEach { points ->
+        queue.add(points)
+      }
+
+      println("${queue.size}")
+
     }
     return result
   }
