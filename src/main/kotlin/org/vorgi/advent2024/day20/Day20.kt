4ec95c46c19b9f0294833aad300e07501d8a3094
voa9bh <andreas.vorgeitz@me.com>
2024-12-20 15:24:04 +0100

    ok, got part 1

--- src/main/kotlin/org/vorgi/advent2024/day20/Day20.kt
+++ src/main/kotlin/org/vorgi/advent2024/day20/Day20.kt
@@ -4,29 +4,53 @@ import org.vorgi.org.vorgi.Utils
 import org.vorgi.org.vorgi.advent2024.day6.CharGrid
 import org.vorgi.org.vorgi.advent2024.day6.Direction
 import org.vorgi.org.vorgi.advent2024.day6.Point
+import kotlin.math.abs
+
+class DistPoint(x:Int , y:Int ,var dist: Int) : Point(x,y) {
+
+  constructor(point:Point ) : this(point,0)
+  constructor(point:Point, dist:Int ) : this(point.x,point.y,dist)
+
+  override operator fun plus(dir:Direction) :DistPoint {
+    return DistPoint(dir.dx,dir.dy,1)+this
+  }
+
+  operator fun plus(otherPoint: DistPoint): DistPoint {
+    return DistPoint(this.x+otherPoint.x,this.y+otherPoint.y,this.dist+otherPoint.dist)
+  }
+
+  override fun toString(): String {
+    return super.toString()+",$dist"
+  }
+}
 
 class Day20 {
   fun start() {
     val input1= Utils.readInput("day20_1")
-    val result1=findCheats(input1,20)
+    val result1=findCheats(input1, 2, 2)
 
     println("result1 = ${result1}")
 
     val input2 = Utils.readInput("day20_2")
-    val result2 = findCheats(input2,100)
+    val result2 = findCheats(input2, 100, 2)
 
-    println("result2 = ${result2}")
+    println("result2 = ${result2.size}")
+
+    val result3=findCheats(input1,50,20)
+
+    val result3Map= mutableMapOf<Int,Int>()
+    for(value in result3) {
+      val count=result3Map.getOrDefault(value,0)+1
+      result3Map[value] = count
+    }
+    println("result3Map = ${result3Map}")
+
   }
 
-  private fun findCheats(input: List<String>, maxShort: Int): List<Int> {
+  private fun findCheats(input: List<String>, minShort: Int, cheatSize: Int=2): List<Int> {
     val maze=CharGrid(input)
 
-    val startPoint = maze.find { x, y -> maze.getAt(x, y) == 'S' }
-    val endPoint = maze.find {x,y -> maze.getAt(x,y)=='E'}
-
-    if(startPoint==null) {
-      throw UnsupportedOperationException("no start")
-    }
+    val startPoint = DistPoint(maze.find { x, y -> maze.getAt(x, y) == 'S' }!!)
 
     val paths = findPath(startPoint, maze)
 
@@ -36,36 +60,38 @@ class Day20 {
 
     println(maze)
 
-    return paths.map { it.size }
-  }
+    val result=mutableListOf<Pair<DistPoint,DistPoint>>()
 
-  private fun findPath(startPoint: Point, maze: CharGrid): List<List<Point>> {
-    val queue: MutableList<Pair<Point, List<Point>>> = mutableListOf()
-    queue.add(Pair(startPoint, listOf(startPoint)))
-    val results = mutableListOf<List<Point>>()
-
-    while (queue.isNotEmpty()) {
-      val currentList = queue.toList()
-      queue.clear()
-      for(element in currentList) {
-        val (currentPoint, currentPath) = element
-
-        if (maze.getAt(currentPoint) == 'E') {
-          results.add(currentPath)
-          continue
-        }
-
-        for (direction in Direction.entries) {
-          val newPoint = currentPoint + direction
-          val newPath = currentPath + newPoint
-          if (maze.getAt(newPoint) == '.' && !currentPath.contains(newPoint)) {
-            queue.add(Pair(newPoint, newPath))
-          }
+    for(point in paths[0]) {
+      val distPoints = paths[0].filter { otherPoint ->
+        abs(point.x - otherPoint.x) + abs(point.y - otherPoint.y) <= cheatSize && otherPoint.dist - point.dist >= minShort + cheatSize
+      }
+      if(distPoints.isNotEmpty()) {
+        distPoints.forEach {
+          result.add(Pair(point,it))
         }
       }
     }
 
-    return results
+    return result.map { it.second.dist - it.first.dist- cheatSize }
+  }
+
+  private fun findPath(startPoint: DistPoint, maze: CharGrid, visited: List<DistPoint> = listOf(startPoint)): List<List<DistPoint>> {
+
+    val result= mutableListOf<List<DistPoint>>()
+
+    for(direction in Direction.entries) {
+      val newPoint = startPoint + direction
+      val newVisited=visited+newPoint
+      if(maze.getAt(newPoint)=='.' && !visited.contains(newPoint)) {
+        result += findPath(newPoint,maze, newVisited)
+      }
+      if(maze.getAt(newPoint)=='E') {
+        return listOf(newVisited)
+      }
+    }
+
+    return result
   }
 }
 
--- src/main/kotlin/org/vorgi/advent2024/day6/Day6.kt
+++ src/main/kotlin/org/vorgi/advent2024/day6/Day6.kt
@@ -33,6 +33,30 @@ open class Point(var x: Int, var y: Int) {
 
     return null
   }
+
+
+
+  override fun toString(): String {
+    return "$x,$y"
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as Point
+
+    if (x != other.x) return false
+    if (y != other.y) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = x
+    result = 31 * result + y
+    return result
+  }
 }
 
 open class CharGrid(input: List<String>) {
